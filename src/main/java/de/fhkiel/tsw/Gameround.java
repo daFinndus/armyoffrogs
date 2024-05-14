9d8bb1c4712b2bbc1e9290d84c9453e7b7097791
daFinndus <finnluca555@gmail.com>
2024-05-14 17:27:24 +0200

    Changed gameround

--- src/main/java/de/fhkiel/tsw/Gameround.java
+++ src/main/java/de/fhkiel/tsw/Gameround.java
@@ -3,16 +3,4 @@ package de.fhkiel.tsw;
 import de.fhkiel.tsw.armyoffrogs.Color;
 
 public class Gameround {
-    private Gamelogic logic;
-
-    public String action = "bewegen";
-    public int round;
-
-    public Color getCurrentPlayer() {
-        return logic.players()[0];
-    }
-
-    public String getActionName() {
-        return action;
-    }
 }
