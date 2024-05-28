5e182684738188f992bbabe05518203c523ea781
Jon4xos <jonas.a.rauchardt@student.fh-kiel.de>
2024-05-28 17:00:25 +0200

    fixed Checkstyle for Bag and Playlogic

--- src/main/java/de/fhkiel/tsw/Bag.java
+++ src/main/java/de/fhkiel/tsw/Bag.java
@@ -1,53 +1,68 @@
 package de.fhkiel.tsw;
 
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.List;
+import java.util.Random;
 
 
+/**
+ * The Bag class represents the bag of frogs.
+ */
 public class Bag {
-    Random random = new Random();
-    private List<Frog> frogs;
+  Random random = new Random();
+  private List<Frog> frogs;
 
-    boolean gameRunning = false;
+  boolean gameRunning = false;
 
 
-    public Bag() {
-        this.frogs = new ArrayList<>();
+  public Bag() {
+    this.frogs = new ArrayList<>();
+  }
+
+  public void setGameRunning(boolean gameRunning) {
+    this.gameRunning = gameRunning;
+  }
+
+  /**
+   * Returns the number of frogs in the bag.
+   */
+  // Returns the number of frogs in the bag
+  public int getNumberOfFrogs() {
+    if (gameRunning) {
+      return frogs.size();
+    } else {
+      return 40;
     }
+  }
 
-    public void setGameRunning(boolean gameRunning) {
-        this.gameRunning = gameRunning;
+  /**
+   * Returns the list of frogs in the bag.
+   */
+  public List<Frog> getFrogList() {
+    return frogs;
+  }
+
+  /**
+   * Removes a frog from the bag and returns it.
+   */
+  // Removes a frog from the bag and returns it
+  public Frog takeFrog() {
+    if (!frogs.isEmpty()) {
+      Frog frog = frogs.get(random.nextInt(frogs.size()));
+      frogs.remove(random.nextInt(frogs.size()));
+
+      return frog;
     }
+    return null;
+  }
 
-    // Returns the number of frogs in the bag
-    public int getNumberOfFrogs() {
-        if (gameRunning) {
-            return frogs.size();
-        } else {
-            return 40;
-        }
+  /**
+   * Adds a frog to the bag.
+   */
+  // Adds a frog to the bag
+  public void putFrog(Frog frog) {
+    if (frogs.size() < 40) {
+      frogs.add(frog);
     }
-
-    public List<Frog> getFrogList() {
-        return frogs;
-    }
-
-    // Removes a frog from the bag and returns it
-    public Frog takeFrog() {
-        if (!frogs.isEmpty()) {
-            Frog frog = frogs.get(random.nextInt(frogs.size()));
-            frogs.remove(random.nextInt(frogs.size()));
-
-            return frog;
-        }
-        return null;
-    }
-
-    // Adds a frog to the bag
-    public void putFrog(Frog frog) {
-        if (frogs.size() < 40) {
-            frogs.add(frog);
-        }
-    }
+  }
 }
--- src/main/java/de/fhkiel/tsw/Playlogic.java
+++ src/main/java/de/fhkiel/tsw/Playlogic.java
@@ -2,11 +2,22 @@ package de.fhkiel.tsw;
 
 import de.fhkiel.tsw.armyoffrogs.Position;
 
-public class Playlogic {
+/**
+ * Die Playlogic Klasse enthält die Spiellogik für das Bewegen von Fröschen.
+ * Sie bietet eine Methode an, um zu überprüfen, ob ein Zug erlaubt ist.
+ */
 
-    public boolean isMoveAllowed(Frog frog, Position position) {
-        // Implement the logic to check if the move is allowed
-        return true;
-        // Return true if the move is allowed, false otherwise
-    }
+public class Playlogic {
+  /**
+   * Check if the move is allowed.
+   *
+   * @param frog     The frog that is moved
+   * @param position The position the frog is moved to
+   * @return true if the move is allowed, false otherwise
+   */
+  public boolean isMoveAllowed(Frog frog, Position position) {
+    // Implement the logic to check if the move is allowed
+    return true;
+    // Return true if the move is allowed, false otherwise
+  }
 }
